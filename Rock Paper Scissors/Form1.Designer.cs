1b830804063ee5c7387365c97839146527086bea
MyComputerBlewUp <99418457+ColeHatesComputers@users.noreply.github.com>
2022-06-21 10:57:27 -0400

    Done

--- Rock Paper Scissors/Form1.Designer.cs
+++ Rock Paper Scissors/Form1.Designer.cs
@@ -28,32 +28,29 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
-            this.pictureBox2 = new System.Windows.Forms.PictureBox();
             this.label1 = new System.Windows.Forms.Label();
             this.Startbtn = new System.Windows.Forms.Button();
-            this.radioButton1 = new System.Windows.Forms.RadioButton();
-            this.radioButton2 = new System.Windows.Forms.RadioButton();
-            this.radioButton3 = new System.Windows.Forms.RadioButton();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.radRock = new System.Windows.Forms.RadioButton();
+            this.radPaper = new System.Windows.Forms.RadioButton();
+            this.radScissors = new System.Windows.Forms.RadioButton();
+            this.label2 = new System.Windows.Forms.Label();
+            this.grpChoices = new System.Windows.Forms.GroupBox();
+            this.lblScore = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.lblWins = new System.Windows.Forms.Label();
+            this.lblLoses = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.imgOpponent = new System.Windows.Forms.PictureBox();
+            this.imgPlayer = new System.Windows.Forms.PictureBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.lblTies = new System.Windows.Forms.Label();
+            this.lblMessage = new System.Windows.Forms.Label();
+            this.grpChoices.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.imgOpponent)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.imgPlayer)).BeginInit();
             this.SuspendLayout();
-            // 
-            // pictureBox1
-            // 
-            this.pictureBox1.Location = new System.Drawing.Point(130, 135);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(160, 151);
-            this.pictureBox1.TabIndex = 0;
-            this.pictureBox1.TabStop = false;
-            // 
-            // pictureBox2
-            // 
-            this.pictureBox2.Location = new System.Drawing.Point(521, 138);
-            this.pictureBox2.Name = "pictureBox2";
-            this.pictureBox2.Size = new System.Drawing.Size(166, 148);
-            this.pictureBox2.TabIndex = 1;
-            this.pictureBox2.TabStop = false;
             // 
             // label1
             // 
@@ -67,65 +64,229 @@
             // 
             // Startbtn
             // 
+            this.Startbtn.BackColor = System.Drawing.Color.GhostWhite;
             this.Startbtn.Font = new System.Drawing.Font("Showcard Gothic", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.Startbtn.Location = new System.Drawing.Point(350, 307);
+            this.Startbtn.Location = new System.Drawing.Point(342, 307);
             this.Startbtn.Name = "Startbtn";
             this.Startbtn.Size = new System.Drawing.Size(94, 34);
             this.Startbtn.TabIndex = 3;
             this.Startbtn.Text = "Start";
-            this.Startbtn.UseVisualStyleBackColor = true;
+            this.Startbtn.UseVisualStyleBackColor = false;
+            this.Startbtn.Click += new System.EventHandler(this.Startbtn_Click);
             // 
-            // radioButton1
+            // radRock
             // 
-            this.radioButton1.AutoSize = true;
-            this.radioButton1.Location = new System.Drawing.Point(130, 376);
-            this.radioButton1.Name = "radioButton1";
-            this.radioButton1.Size = new System.Drawing.Size(85, 17);
-            this.radioButton1.TabIndex = 4;
-            this.radioButton1.TabStop = true;
-            this.radioButton1.Text = "radioButton1";
-            this.radioButton1.UseVisualStyleBackColor = true;
+            this.radRock.AutoSize = true;
+            this.radRock.Font = new System.Drawing.Font("Showcard Gothic", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.radRock.Location = new System.Drawing.Point(6, 32);
+            this.radRock.Name = "radRock";
+            this.radRock.Size = new System.Drawing.Size(96, 34);
+            this.radRock.TabIndex = 4;
+            this.radRock.TabStop = true;
+            this.radRock.Text = "Rock";
+            this.radRock.UseVisualStyleBackColor = true;
+            this.radRock.CheckedChanged += new System.EventHandler(this.radRock_CheckedChanged);
             // 
-            // radioButton2
+            // radPaper
             // 
-            this.radioButton2.AutoSize = true;
-            this.radioButton2.Location = new System.Drawing.Point(316, 376);
-            this.radioButton2.Name = "radioButton2";
-            this.radioButton2.Size = new System.Drawing.Size(85, 17);
-            this.radioButton2.TabIndex = 5;
-            this.radioButton2.TabStop = true;
-            this.radioButton2.Text = "radioButton2";
-            this.radioButton2.UseVisualStyleBackColor = true;
+            this.radPaper.AutoSize = true;
+            this.radPaper.BackColor = System.Drawing.Color.DarkTurquoise;
+            this.radPaper.Font = new System.Drawing.Font("Showcard Gothic", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.radPaper.Location = new System.Drawing.Point(6, 72);
+            this.radPaper.Name = "radPaper";
+            this.radPaper.Size = new System.Drawing.Size(107, 34);
+            this.radPaper.TabIndex = 5;
+            this.radPaper.TabStop = true;
+            this.radPaper.Text = "Paper";
+            this.radPaper.UseVisualStyleBackColor = false;
+            this.radPaper.CheckedChanged += new System.EventHandler(this.radPaper_CheckedChanged);
             // 
-            // radioButton3
+            // radScissors
             // 
-            this.radioButton3.AutoSize = true;
-            this.radioButton3.Location = new System.Drawing.Point(521, 376);
-            this.radioButton3.Name = "radioButton3";
-            this.radioButton3.Size = new System.Drawing.Size(85, 17);
-            this.radioButton3.TabIndex = 6;
-            this.radioButton3.TabStop = true;
-            this.radioButton3.Text = "radioButton3";
-            this.radioButton3.UseVisualStyleBackColor = true;
+            this.radScissors.AutoSize = true;
+            this.radScissors.Font = new System.Drawing.Font("Showcard Gothic", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.radScissors.Location = new System.Drawing.Point(6, 112);
+            this.radScissors.Name = "radScissors";
+            this.radScissors.Size = new System.Drawing.Size(134, 34);
+            this.radScissors.TabIndex = 6;
+            this.radScissors.TabStop = true;
+            this.radScissors.Text = "Scissors";
+            this.radScissors.UseVisualStyleBackColor = true;
+            this.radScissors.CheckedChanged += new System.EventHandler(this.radScissors_CheckedChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Showcard Gothic", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(12, 26);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(781, 79);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Rock, Paper, Scissors!";
+            // 
+            // grpChoices
+            // 
+            this.grpChoices.Controls.Add(this.radRock);
+            this.grpChoices.Controls.Add(this.radPaper);
+            this.grpChoices.Controls.Add(this.radScissors);
+            this.grpChoices.Font = new System.Drawing.Font("Showcard Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.grpChoices.Location = new System.Drawing.Point(63, 292);
+            this.grpChoices.Name = "grpChoices";
+            this.grpChoices.Size = new System.Drawing.Size(160, 155);
+            this.grpChoices.TabIndex = 8;
+            this.grpChoices.TabStop = false;
+            this.grpChoices.Text = "Choices";
+            // 
+            // lblScore
+            // 
+            this.lblScore.AutoSize = true;
+            this.lblScore.Font = new System.Drawing.Font("Showcard Gothic", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblScore.Location = new System.Drawing.Point(460, 355);
+            this.lblScore.Name = "lblScore";
+            this.lblScore.Size = new System.Drawing.Size(93, 30);
+            this.lblScore.TabIndex = 9;
+            this.lblScore.Text = "Score:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Showcard Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(559, 324);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(70, 20);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "Player";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Showcard Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(662, 324);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(42, 20);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "Bot";
+            // 
+            // lblWins
+            // 
+            this.lblWins.AutoSize = true;
+            this.lblWins.Font = new System.Drawing.Font("Showcard Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblWins.Location = new System.Drawing.Point(583, 365);
+            this.lblWins.Name = "lblWins";
+            this.lblWins.Size = new System.Drawing.Size(18, 20);
+            this.lblWins.TabIndex = 12;
+            this.lblWins.Text = "0";
+            // 
+            // lblLoses
+            // 
+            this.lblLoses.AutoSize = true;
+            this.lblLoses.Font = new System.Drawing.Font("Showcard Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblLoses.Location = new System.Drawing.Point(675, 364);
+            this.lblLoses.Name = "lblLoses";
+            this.lblLoses.Size = new System.Drawing.Size(18, 20);
+            this.lblLoses.TabIndex = 13;
+            this.lblLoses.Text = "0";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 72F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(607, 307);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(73, 108);
+            this.label5.TabIndex = 14;
+            this.label5.Text = "I";
+            // 
+            // imgOpponent
+            // 
+            this.imgOpponent.Image = global::Rock_Paper_Scissors.Properties.Resources.Start;
+            this.imgOpponent.Location = new System.Drawing.Point(521, 138);
+            this.imgOpponent.Name = "imgOpponent";
+            this.imgOpponent.Size = new System.Drawing.Size(166, 148);
+            this.imgOpponent.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.imgOpponent.TabIndex = 1;
+            this.imgOpponent.TabStop = false;
+            // 
+            // imgPlayer
+            // 
+            this.imgPlayer.Image = global::Rock_Paper_Scissors.Properties.Resources.Start;
+            this.imgPlayer.Location = new System.Drawing.Point(130, 135);
+            this.imgPlayer.Name = "imgPlayer";
+            this.imgPlayer.Size = new System.Drawing.Size(160, 151);
+            this.imgPlayer.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.imgPlayer.TabIndex = 0;
+            this.imgPlayer.TabStop = false;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 72F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(699, 307);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(73, 108);
+            this.label7.TabIndex = 16;
+            this.label7.Text = "I";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Showcard Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(756, 324);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(43, 20);
+            this.label6.TabIndex = 17;
+            this.label6.Text = "Ties";
+            // 
+            // lblTies
+            // 
+            this.lblTies.AutoSize = true;
+            this.lblTies.Font = new System.Drawing.Font("Showcard Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTies.Location = new System.Drawing.Point(765, 365);
+            this.lblTies.Name = "lblTies";
+            this.lblTies.Size = new System.Drawing.Size(18, 20);
+            this.lblTies.TabIndex = 18;
+            this.lblTies.Text = "0";
+            // 
+            // lblMessage
+            // 
+            this.lblMessage.AutoSize = true;
+            this.lblMessage.Font = new System.Drawing.Font("Showcard Gothic", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblMessage.Location = new System.Drawing.Point(458, 404);
+            this.lblMessage.Name = "lblMessage";
+            this.lblMessage.Size = new System.Drawing.Size(95, 27);
+            this.lblMessage.TabIndex = 19;
+            this.lblMessage.Text = "Status:";
             // 
             // RPS
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.DarkTurquoise;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.radioButton3);
-            this.Controls.Add(this.radioButton2);
-            this.Controls.Add(this.radioButton1);
+            this.ClientSize = new System.Drawing.Size(816, 450);
+            this.Controls.Add(this.lblMessage);
+            this.Controls.Add(this.lblTies);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.lblLoses);
+            this.Controls.Add(this.lblWins);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.lblScore);
+            this.Controls.Add(this.grpChoices);
+            this.Controls.Add(this.label2);
             this.Controls.Add(this.Startbtn);
             this.Controls.Add(this.label1);
-            this.Controls.Add(this.pictureBox2);
-            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.imgOpponent);
+            this.Controls.Add(this.imgPlayer);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label7);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
             this.Name = "RPS";
-            this.Text = "Rock, Paper, Scissors";
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.Text = "RPS                                                  ";
+            this.Load += new System.EventHandler(this.RPS_Load);
+            this.grpChoices.ResumeLayout(false);
+            this.grpChoices.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.imgOpponent)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.imgPlayer)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -133,13 +294,25 @@
 
         #endregion
 
-        private System.Windows.Forms.PictureBox pictureBox1;
-        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox imgPlayer;
+        private System.Windows.Forms.PictureBox imgOpponent;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Button Startbtn;
-        private System.Windows.Forms.RadioButton radioButton1;
-        private System.Windows.Forms.RadioButton radioButton2;
-        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.RadioButton radRock;
+        private System.Windows.Forms.RadioButton radPaper;
+        private System.Windows.Forms.RadioButton radScissors;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.GroupBox grpChoices;
+        private System.Windows.Forms.Label lblScore;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label lblWins;
+        private System.Windows.Forms.Label lblLoses;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label lblTies;
+        private System.Windows.Forms.Label lblMessage;
     }
 }
 
--- Rock Paper Scissors/Form1.cs
+++ Rock Paper Scissors/Form1.cs
@@ -12,11 +12,92 @@ namespace Rock_Paper_Scissors
 {
     public partial class RPS : Form
     {
+        Random generator = new Random();
+        int win = 0;
+        int tie = 0;
+        int lose = 0;
+         
         public RPS()
         {
             InitializeComponent();
         }
 
-       
+        private void RPS_Load(object sender, EventArgs e)
+        {
+            radRock.Checked = false;
+            imgPlayer.Image = Properties.Resources.Start;
+        }
+        private void radRock_CheckedChanged(object sender, EventArgs e)
+        {
+            imgPlayer.Image = Properties.Resources.Rock;
+        }
+
+        private void radPaper_CheckedChanged(object sender, EventArgs e)
+        {
+            imgPlayer.Image = Properties.Resources.Paper;
+        }
+
+        private void radScissors_CheckedChanged(object sender, EventArgs e)
+        {
+            imgPlayer.Image = Properties.Resources.Scissors;
+        }
+        
+        private void Startbtn_Click(object sender, EventArgs e)
+        {
+            int random = generator.Next(1, 4);
+            if (random == 1)
+                imgOpponent.Image = Properties.Resources.Rock;
+            
+            else if (random == 2)
+                imgOpponent.Image = Properties.Resources.Paper;
+            else if (random == 3)
+                imgOpponent.Image = Properties.Resources.Scissors;
+            if (radRock.Checked && random == 3)
+                lblMessage.Text = ("Status: Winner!");
+            
+            else if (radRock.Checked && random == 2)
+                lblMessage.Text = ("Status: Loser!");
+            
+            else if (radRock.Checked && random == 1)
+                lblMessage.Text = ("Status: Tie!");
+            
+            else if (radPaper.Checked && random == 3)
+                lblMessage.Text = ("Status: Loser!");
+            
+            else if (radPaper.Checked && random == 2)
+                lblMessage.Text = ("Status: Tie!");
+            
+            else if (radPaper.Checked && random == 1)
+                lblMessage.Text = ("Status: Winner!");
+            
+            else if (radScissors.Checked && random == 3)
+                lblMessage.Text = ("Status: Tie!");
+            
+            else if (radScissors.Checked && random == 2)
+                lblMessage.Text = ("Status: Winner!");
+            
+            else if (radScissors.Checked && random == 1)
+                lblMessage.Text = ("Status: Loser!");
+            if (radRock.Checked && random == 1 || radPaper.Checked && random == 2 || radScissors.Checked && random == 3)
+                tie = tie + 1;
+            
+            else if (radRock.Checked && random == 2 || radPaper.Checked && random == 3 || radScissors.Checked && random == 1)
+                lose = lose + 1;
+            
+            else if (radRock.Checked && random == 3 || radPaper.Checked && random == 1 || radScissors.Checked && random == 2)
+                win = win + 1;
+            lblTies.Text = Convert.ToString(tie);
+            
+            lblLoses.Text = Convert.ToString(lose);
+            
+            lblWins.Text = Convert.ToString(win);
+            
+            radRock.Checked = false;
+            radPaper.Checked = false;
+            radScissors.Checked = false;
+        }
+
+        
     }
 }
+
